10653b8839478a107b5c813efc6b0c5378badd87
Yasuyuki Tanaka <yatch@users.noreply.github.com>
2018-12-21 12:18:28 +0100

    Add config-ws-butler.el

---
+++ personal/config-ws-butler.el
@@ -0,0 +1,8 @@
+(prelude-require-package 'ws-butler)
+
+(add-hook 'c-mode-common-hook 'ws-butler-mode)
+(add-hook 'python-mode-hook 'ws-butler-mode)
+(add-hook 'js2-mode-hook 'ws-butler-mode)
+(add-hook 'org-mode-hook 'ws-butler-mode)
+(add-hook 'html-mode-hook 'ws-butler-mode)
+(add-hook 'web-mode-hook 'ws-butler-mode)
