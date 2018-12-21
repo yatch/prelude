8b4f755124a2fb33ed63f3e774ebfe3853ab9ca4
Yasuyuki Tanaka <yatch@users.noreply.github.com>
2018-12-21 12:18:06 +0100

    Add config-keybindings.el

---
+++ personal/config-keybindings.el
@@ -0,0 +1,7 @@
+;; https://www.emacswiki.org/emacs/BackspaceKey
+
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "M-?") 'mark-paragraph)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+(global-set-key (kbd "C-m") 'newline-and-indent)
