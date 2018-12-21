701965f693cafa6e10461b42763dac5ea28dfed0
Yasuyuki Tanaka <yatch@users.noreply.github.com>
2018-12-21 12:19:10 +0100

    Add config-appearance.el

---
+++ personal/config-appearance.el
@@ -0,0 +1,14 @@
+(show-paren-mode t)
+(blink-cursor-mode 0)
+(if window-system
+    (progn
+      (tool-bar-mode 0)
+      (scroll-bar-mode 0)))
+(line-number-mode 1)
+(column-number-mode 1)
+(global-hl-line-mode t)
+(setq inhibit-startup-message t)
+(setq use-dialog-box nil)
+(show-paren-mode t)
+(global-linum-mode 1)
+(setq linum-format "%4d ")
