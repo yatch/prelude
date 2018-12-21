4f2523cdc18ad14f1708f8956fa3a281c66efe78
Yasuyuki Tanaka <yatch@users.noreply.github.com>
2018-12-21 12:18:13 +0100

    Add config-multiple-cursors.el

---
+++ personal/config-multiple-cursorts.el
@@ -0,0 +1,6 @@
+(prelude-require-package 'multiple-cursors)
+
+(global-set-key (kbd "C-c C-m") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
