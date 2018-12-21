eb57cb6c3ce0aa478927ed87176efea057050039
Yasuyuki Tanaka <yatch@users.noreply.github.com>
2018-12-21 12:18:21 +0100

    Add config-whitespace-mode.el

---
+++ personal/config-whitespace-mode.el
@@ -0,0 +1,10 @@
+(setq whitespace-style '(empty
+                         face
+                         tabs
+                         trailing
+                         lines-tail
+                         indentation
+                         space-before-tab
+                         space-after-tab))
+(global-whitespace-mode 1)
+(setq whitespace-line-column 80) ; set back the default value, 80
